87d4a07862450662986b3927f02352b82d26be54
Deepika Reddy <deepika.weeb@gmail.com>
2026-02-17 00:43:53 +0530

    Deepika Added: OOPSBanner main application

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -2,8 +2,7 @@ public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        // UC1: Print literal text OOPS
-        System.out.println("OOPS");
+        System.out.println("Welcome to OOPSBanner application");
 
     }
 }
